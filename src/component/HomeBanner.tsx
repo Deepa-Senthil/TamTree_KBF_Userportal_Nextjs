981cc3cfbf28d9985b4e56704aa3cab7cfd1024e
Deepa-Senthil <deepasenthil5812@gmail.com>
2025-07-09 12:46:23 +0530

    about us page created

--- src/component/HomeBanner.tsx
+++ src/component/HomeBanner.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
 
-function HomeBanner() {
+import Slider from "react-slick";
+import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "../styles/HomeReview.module.scss";
+
+const HomeBanner = () => {
+  const images = [
+    "/asset/images/Kbf-home-banner1.jpg",
+    "/asset/images/Kbf-home-banner2.jpg",
+    "/asset/images/Kbf-home-banner3.jpg",
+    "/asset/images/Kbf-home-banner4.jpg",
+  ];
+
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: true,
+        },
+      },
+    ],
+  };
+
   return (
-    <div>HomeBanner</div>
-  )
-}
+    <div className={styles.bannerWrapper}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className={styles.slide}>
+            <img
+              src={image}
+              alt={`Banner ${index + 1}`}
+              
+              className={styles.bannerImage}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
