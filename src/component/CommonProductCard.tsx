ddb6f698e7f48b163e707d7eb2caedfb31dbebbe
Deepa-Senthil <deepasenthil5812@gmail.com>
2025-07-15 09:11:30 +0530

    common product card alignment changes

--- src/component/CommonProductCard.tsx
+++ src/component/CommonProductCard.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-// import AddToBagModel from "./AddToBagModel";
-// import { useCart } from "@/context/CartContext";
 import styles from "../styles/CommonProductCard.module.scss";
-
 
 interface Product {
   id: number;
@@ -17,7 +14,6 @@ interface Product {
 
 export default function CommonProductCard({ product }: { product: Product }) {
   const router = useRouter();
-//   const { addToCart, cartItems } = useCart();
 
   const [selectedSize, setSelectedSize] = useState(
     product.sizes[0]?.size || ""
@@ -25,22 +21,8 @@ export default function CommonProductCard({ product }: { product: Product }) {
   const [selectedPrice, setSelectedPrice] = useState(
     product.sizes[0]?.price || 0
   );
-  const [openModal, setOpenModal] = useState(false);
-  const [modelData, setModelData] = useState<any>(null);
   const [quantity, setQuantity] = useState(1);
   const MAX_QUANTITY = 20;
-
-//   useEffect(() => {
-//     const existingItem = cartItems.find(
-//       (item) =>
-//         item.id === product.id.toString() && item.selectedSize === selectedSize
-//     );
-//     if (existingItem) {
-//       setQuantity(existingItem.Quantity);
-//     } else {
-//       setQuantity(1);
-//     }
-//   }, [cartItems, product.id, selectedSize]);
 
   const handleSizeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const size = e.target.value;
@@ -48,25 +30,6 @@ export default function CommonProductCard({ product }: { product: Product }) {
     setSelectedSize(size);
     setSelectedPrice(found?.price || 0);
   };
-
-//   const handleAddToCart = () => {
-//     if (quantity <= 0) {
-//       alert("Minimum quantity is 1.");
-//       return;
-//     }
-
-//     const selectedProduct = {
-//       ...product,
-//       selectedSize,
-//       selectedPrice,
-//       Quantity: quantity,
-//       id: product.id.toString(),
-//     };
-
-//     addToCart(selectedProduct);
-//     setOpenModal(true);
-//     setModelData(selectedProduct);
-//   };
 
   const handleIncreaseQuantity = () => {
     if (quantity < MAX_QUANTITY) {
@@ -88,68 +51,55 @@ export default function CommonProductCard({ product }: { product: Product }) {
   };
 
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.cardImage} onClick={handleCardClick}>
-          <Image
-            src={product.imageUrl}
-            alt={product.title}
-            width={300}
-            height={220}
-            layout="responsive"
-          />
-        </div>
-
-        <div className={styles.cardContent}>
-          <h3 className={styles.title}>{product.title}</h3>
-
-          <select
-            className={styles.select}
-            value={selectedSize}
-            onChange={handleSizeChange}
-          >
-            {product.sizes.map((item, index) => (
-              <option key={index} value={item.size}>
-                {item.size} - ₹{item.price}
-              </option>
-            ))}
-          </select>
-
-          <div className={styles.quantityWrapper}>
-            <div className={styles.quantityBox}>
-              <button
-                onClick={handleDecreaseQuantity}
-                disabled={quantity === 1}
-              >
-                -
-              </button>
-              <input
-                type="number"
-                value={quantity}
-                min={1}
-                max={MAX_QUANTITY}
-                onChange={(e) => {
-                  const val = parseInt(e.target.value);
-                  if (!isNaN(val) && val >= 1 && val <= MAX_QUANTITY) {
-                    setQuantity(val);
-                  }
-                }}
-              />
-              <button onClick={handleIncreaseQuantity}>+</button>
-            </div>
-o
-            <button >
-              🛒
-            </button>
-          </div>
-        </div>
+    <div className={styles.card}>
+      <div className={styles.cardImage} onClick={handleCardClick}>
+        <Image
+          src={product.imageUrl}
+          alt={product.title}
+          fill
+          style={{ objectFit: "cover" }}
+          className={styles.bannerImage}
+        />
       </div>
 
-      {/* <AddToBagModel
-        open={openModal}
-        onClose={() => setOpenModal(false)}
-        product={modelData}
-      /> */}
-    </>
+      <div className={styles.cardContent}>
+        <h3 className={styles.title}>{product.title}</h3>
+
+        <select
+          className={styles.select}
+          value={selectedSize}
+          onChange={handleSizeChange}
+        >
+          {product.sizes.map((item, index) => (
+            <option key={index} value={item.size}>
+              {item.size}&nbsp; - &nbsp; ₹{item.price}
+            </option>
+          ))}
+        </select>
+
+        <div className={styles.quantityWrapper}>
+          <div className={styles.quantityBox}>
+            <button onClick={handleDecreaseQuantity} disabled={quantity === 1}>
+              -
+            </button>
+            <input
+            //   type="number"
+              value={quantity}
+              min={1}
+              max={MAX_QUANTITY}
+              onChange={(e) => {
+                const val = parseInt(e.target.value);
+                if (!isNaN(val) && val >= 1 && val <= MAX_QUANTITY) {
+                  setQuantity(val);
+                }
+              }}
+            />
+            <button onClick={handleIncreaseQuantity}>+</button>
+          </div>
+
+          <button className={styles.addToCart}>🛒</button>
+        </div>
+      </div>
+    </div>
   );
 }
